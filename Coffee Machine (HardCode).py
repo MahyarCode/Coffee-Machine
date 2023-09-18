d756348430b8af348a511e141bd4c70e2faca792
MahyarCode <mahyar.mousavi.1408@gmail.com>
2023-09-18 17:36:34 +0330

    created files

---
+++ Coffee Machine (HardCode).py
@@ -0,0 +1,145 @@
+sources = {"Coffee" : 100,
+            "Water": 300,
+            "Milk" : 200}
+
+espresso = {"Coffee": 18,
+                "Water":50}
+
+latte = {"Coffee": 24,
+                "Water":200,
+                "Milk": 150}
+
+cappuccino = {"Coffee": 24,
+                "Water":250,
+                "Milk": 100}
+
+penny = 0.01
+nickel = 0.05
+dime = 0.10
+quarter = 0.25
+
+espresso_Price = 1.5
+cappuccino_Price = 2.0
+latte_Price = 2.5
+
+while True:
+    user = input("What would you like? (espresso/latte/cappuccino): ")
+
+    choose = ["espresso" , "latte" , "cappuccino"]
+
+    if user not in choose and user != "report" and user != "off":
+        print("invalid input")
+
+    if user == "off":
+        break
+
+    if user == "report":
+        print(f"Coffee amount is: {sources['Coffee']}g\n"
+              f"Water amount is: {sources['Water']}ml\n"
+              f"Milk amount is: {sources['Milk']}ml\n")
+    
+    
+    
+
+    
+
+
+
+    ## user input and calculation of resources
+    if user in choose:
+        if user == "espresso":
+            if espresso["Coffee"] <= sources["Coffee"] and espresso["Water"] <= sources["Water"]:
+                penny_num = int(input("how much Penny inserted ? "))
+                nickel_num = int(input("how much Nickel inserted ? "))
+                dime_num = int(input("how much Dime inserted ? "))
+                quarter_num = int(input("how much Quarter inserted ? "))
+                total_cashe = penny*penny_num + nickel*nickel_num + dime* dime_num + quarter*quarter_num
+
+            
+                if total_cashe < espresso_Price:
+                    print("there's not enough money for espresso")
+
+                if total_cashe > espresso_Price:        
+                    sources["Coffee"] = sources["Coffee"] - espresso["Coffee"]
+                    sources["Water"] = sources["Water"] - espresso["Water"]
+                    print(f"here's your change: {total_cashe - espresso_Price}$\n"
+                        f"Enjoy your Espresso <3")
+                                
+                if total_cashe == espresso_Price:            
+                    sources["Coffee"] = sources["Coffee"] - espresso["Coffee"]
+                    sources["Water"] = sources["Water"] - espresso["Water"]
+                    print("Enjoy your Espresso <3")
+
+            elif espresso["Coffee"] > sources["Coffee"]:
+                print("sorry we run out of Coffee")
+            elif espresso["Water"] > sources["Water"]:
+                print("sorry we run out of water")
+                            
+                        
+        if user == "cappuccino":
+            if cappuccino["Coffee"] <= sources["Coffee"] and cappuccino["Water"] <= sources["Water"] and cappuccino["Milk"] <= sources["Milk"]:
+                penny_num = int(input("how much Penny inserted ? "))
+                nickel_num = int(input("how much Nickel inserted ? "))
+                dime_num = int(input("how much Dime inserted ? "))
+                quarter_num = int(input("how much Quarter inserted ? "))
+                total_cashe = penny*penny_num + nickel*nickel_num + dime* dime_num + quarter*quarter_num
+
+
+                if total_cashe < cappuccino_Price:
+                    print("there's not enough money for cappuccino")
+
+                if total_cashe > cappuccino_Price:                
+                    sources["Coffee"] = sources["Coffee"] - cappuccino["Coffee"]
+                    sources["Water"] = sources["Water"] - cappuccino["Water"]
+                    sources["Milk"] = sources["Milk"] - cappuccino["Milk"]
+                    print(f"here's your change: {round(total_cashe - cappuccino_Price,2)}$\n"
+                        f"Enjoy your cappuccino <3")
+                        
+                if total_cashe == cappuccino_Price:
+                        sources["Coffee"] = sources["Coffee"] - cappuccino["Coffee"]
+                        sources["Water"] = sources["Water"] - cappuccino["Water"]
+                        sources["Milk"] = sources["Milk"] - cappuccino["Milk"]
+                        print("Enjoy your cappuccino <3")
+
+            elif cappuccino["Coffee"] > sources["Coffee"]:
+                print("sorry we run out of Coffee")
+            elif cappuccino["Water"] > sources["Water"]:
+                print("sorry we run out of water")
+
+
+        if user == "latte":
+            if latte["Coffee"] <= sources["Coffee"] and latte["Water"] <= sources["Water"] and latte["Milk"] <= sources["Milk"]:
+                penny_num = int(input("how much Penny inserted ? "))
+                nickel_num = int(input("how much Nickel inserted ? "))
+                dime_num = int(input("how much Dime inserted ? "))
+                quarter_num = int(input("how much Quarter inserted ? "))
+                total_cashe = penny*penny_num + nickel*nickel_num + dime* dime_num + quarter*quarter_num
+
+
+                if total_cashe < latte_Price:
+                    print("there's not enough money for latte")
+
+                if total_cashe > latte_Price:               
+                    sources["Coffee"] = sources["Coffee"] - latte["Coffee"]
+                    sources["Water"] = sources["Water"] - latte["Water"]
+                    sources["Milk"] = sources["Milk"] - latte["Milk"]
+
+                    print(f"here's your change: {round(total_cashe - latte_Price,2)}$\n"
+                        f"Enjoy your latte <3")
+                                        
+                if total_cashe == latte_Price:               
+                    sources["Coffee"] = sources["Coffee"] - latte["Coffee"]
+                    sources["Water"] = sources["Water"] - latte["Water"]
+                    sources["Milk"] = sources["Milk"] - latte["Milk"]
+                    print("Enjoy your latte <3")            
+            
+            elif latte["Coffee"] > sources["Coffee"]:
+                print("sorry we run out of Coffee")
+            elif latte["Water"] > sources["Water"]:
+                print("sorry we run out of water")
+
+    if sources["Coffee"] < 18 or sources["Water"] < 50 :
+        print("Coffee machine is out of order")
+        break
+        
+        
---
+++ Coffee Machine (Soft Code).py
@@ -0,0 +1,158 @@
+Resources = {"Coffee" : 100,
+            "Water": 500,
+            "Milk" : 300,
+            "Sugar": 800,
+            "Tea": 200}
+
+Menu = {
+    "Espresso":{
+        "Ingredients":{
+            "Coffee": 18,
+            "Water":50
+        },
+        "Cost":1,
+        "Price":1.5
+    },
+
+    "Cappuccino":{
+        "Ingredients":{
+            "Coffee": 24,
+            "Water":250,
+            "Milk": 100
+        },
+        "Cost":1.5,
+        "Price":2
+    },
+
+    "Latte":{
+        "Ingredients":{
+            "Coffee": 24,
+            "Water":200,
+            "Milk": 150
+        },
+        "Cost":1.75,
+        "Price":2.5
+    },
+
+    "Tea":{
+        "Ingredients":{
+            "Tea": 10,
+            "Water":250,
+            "Sugar": 150
+        },
+        "Cost":1.2,
+        "Price":2.25
+    }
+}
+
+Penny = 0.01
+Nickel = 0.05
+Dime = 0.10
+Quarter = 0.25
+Profit = 0
+
+def isReport(User_Order):
+    if User_Order == "Report": 
+        for Ingredient in Resources:
+            print(f"{Ingredient}: {Resources[Ingredient]}")
+            
+        print(f"The total profit of todays machine is: {Profit}$ ")
+        return True 
+    
+    else:
+        return True
+
+def isMenu(User_Order):
+    if User_Order in Menu:
+        return True
+    
+    elif User_Order == "Off" or User_Order == "Report":
+        pass
+
+    else:
+        print("Invalid input! Please try again")
+            
+def Check_Resources(User_Order):
+    Cooking_items = Menu[User_Order]['Ingredients']
+
+    for Components, quantity in Cooking_items.items():
+        if Resources[Components] < quantity : 
+            print(f"Sorry! There is not enough {Components} ro make {User_Order}")
+            # return False       
+
+def Update_Resources(User_Order):
+    Cooking_items = Menu[User_Order]['Ingredients']
+
+    for Components, quantity in Cooking_items.items():
+        if Resources[Components] >= quantity : 
+            Resources[Components] -= quantity
+            
+
+def CheckMoney_and_UpdateResources(Penny_cash, Nickel_cash, Dime_cash, Quarter_cash):    
+    if isMenu(User_Order):
+        Total_cashe = Penny_cash*0.01 + Nickel_cash*0.05 + Dime_cash*0.10 + Quarter_cash*0.25
+
+        if Total_cashe >= Menu[User_Order]['Price']:
+            print(f"Here is your change: {Total_cashe - Menu[User_Order]['Price']}$ ")
+            print(f"Enjoy your {User_Order}")
+            Update_Resources(User_Order)
+                    
+        elif Total_cashe == Menu[User_Order]['Price']:
+            print(f"Enjoy your{User_Order}")
+            Update_Resources(User_Order)
+                  
+        else:
+            print(f"There not enough money for {User_Order}")
+
+
+Menu_List = []
+for each_item in Menu:
+    Menu_List.append(each_item)
+
+Menu_List = ', '.join(Menu_List)
+
+# The Coffee machine Operation
+while True:
+    # user insert a write item in menu
+    User_Order = input(f"What would you like ? {Menu_List}: ").title()
+    
+    # Check if manager wants to turn off the machine
+    if User_Order == "Off":
+        break
+
+    # Check if manager wants take a report of resources   
+    isReport(User_Order)
+
+    # Check if user insert a write item in menu
+    if isMenu(User_Order):
+        
+### ❌❌❌❌Problem Here 👇👇👇👇👇
+        Check_Resources(User_Order) 
+### ❌❌❌❌Problem Here 👆👆👆👆👆 
+
+########## Problem: Customer must choose an item in coffee machine
+##########          and at first, machine has to check the resources if it is enough make the order or not
+##########          after that if the resource is enough to make the order, ask for cash from the user + Update the resources 
+##########   👇👇   which is down below
+##########          but ❌ i can't use this condition ❌ if the resource is not enough to make
+##########          then ask user for another choose: (back again to the loop and ask user again)
+##########          not to break the whole loop !!!!
+##########          when the source is not enough and user enter the whole price for his order, the resource will update 
+##########          and this will cause problem for report !!!!
+
+        # Check how much money inserted in machine
+        Penny_cash   = int(input("How much Penny inserted? "))
+        Nickel_cash  = int(input("How much Nickel inserted? "))
+        Dime_cash    = int(input("How much Dime inserted? "))
+        Quarter_cash = int(input("How much Quarter inserted? "))
+
+        Order_Price = Menu[User_Order]['Price']
+        Order_Cost = Menu[User_Order]['Cost']
+        Profit += Order_Price - Order_Cost
+
+        # if user inserted enough money in machine and then the machine makes the order and update the resources
+        CheckMoney_and_UpdateResources(Penny_cash, Nickel_cash, Dime_cash, Quarter_cash)
+
+        
+
+
